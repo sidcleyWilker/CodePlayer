998e1b88c760937851ffcb3cb91ccc2e567cbd2b
sidcleywilker <sidcleywilker@gmail.com>
2017-10-28 15:40:23 -0300

    JavaScript feito, e projeto concluido.

--- js/myJquery.js
+++ js/myJquery.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 
+	function updateOutput(){
+		$("iframe").contents().find("html").html(
+    		"<html> <head> <style type='text/css'>" + 
+    		$("#cssPanel").val() + "</style> </head> <body>" +
+    		$("#htmlPanel").val() + "</body> </html>"
+    	);
+
+		document.getElementById("outputPanel").contentWindow.eval($("#javaScriptPanel").val());    	
+	}
+
 	$(".toggleButton").click(function(){
 		$(this).toggleClass("active");
 
@@ -15,15 +25,11 @@ $(document).ready(function(){
 
 	$(".panel").height( $(window).height() - $("#header").height() - 15);
 	$(".panel").width( ($(window).width() / 2) - 10);
-
 	$("iframe").contents().find("html").html($("#htmlPanel").val());
 
+	updateOutput();
 	$("textarea").on('change keyup paste', function() {
-    	$("iframe").contents().find("html").html(
-    		"<html> <head> <style type='text/css'>" + 
-    		$("#cssPanel").val() + "</style> </head> <body>" +
-    		$("#htmlPanel").val() + "</body> </html>"
-    	);
+    	updateOutput();
 	});
 
 });

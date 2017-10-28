3f3b31273278c1ec9793c500259200eb12b68c9f
sidcleywilker <sidcleywilker@gmail.com>
2017-10-28 14:12:37 -0300

    Html funcionando

--- js/myJquery.js
+++ js/myJquery.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
 		$(this).toggleClass("active");
 	});
 
+
+	$(".panel").height( $(window).height() - $("#header").height() - 15);
+	$(".panel").width( ($(window).width() / 2) -5);
+
+	$("iframe").contents().find("html").html($("#panelHtml").val());
+
+	$("textarea").on('change keyup paste', function() {
+    	$("iframe").contents().find("html").html($("#panelHtml").val());
+	});
+
 });
